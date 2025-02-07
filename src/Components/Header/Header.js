45bd8769f62dbbab1ecbf634e28c09af69cd4015
sameer06-sa <sameeruddin343@gmail.comm>
2025-02-07 23:04:31 +0530

    updated forntend

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -122,7 +122,7 @@ function Header() {
     const handleAutoLogin = async (email) => {
         setIsLoading(true);
         try {
-            const response = await axios.post(`http://localhost:3000/api/user/auto-login`, { email });
+            const response = await axios.post(`${apiUrl}/api/user/auto-login`, { email });
 
             if (response.data?.success) {
                 if (response.data.data?.user) {
